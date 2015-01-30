3c32afa4cfbee12dd3995b4cb164eb915dc91685
Olof Johansson <olof.johansson@oddhill.se>
2015-01-30 10:09:16 +0100

    Added initial entry point for the app.

---
+++ index.js
@@ -0,0 +1,15 @@
+// Liad dependencies and initiate express.
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+// Use the body-parser middleware in order to read POST data.
+app.use(bodyParser.json());
+
+// Listen for POST data at the root path.
+app.post('/', function(request, response) {
+  response.send(request.body);
+});
+
+// Start the server.
+app.listen(3000);
