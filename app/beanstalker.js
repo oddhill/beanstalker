7fe652dae30b55f492b42e0b34dd1042a3a0019b
Olof Johansson <olof.johansson@oddhill.se>
2015-04-24 15:18:14 +0200

    Added support for duplicating and dropping databases.

--- app/beanstalker.js
+++ app/beanstalker.js
@@ -1,3 +1,65 @@
+// Load the dependencies.
+var fs = require('fs-extra');
+var exec = require('child_process').exec;
+var mysql = require('mysql');
+var client = require('./client.js');
+var db = require('./db.js');
+
+/**
+ * Copies a database and the entire content.
+ *
+ * @param string baseDatabase
+ *   The name of the database that should get cloned.
+ * @param string newDatabase
+ *   The name of the new database.
+ * @param string dumpPath
+ *   The path to the folder where we should store the SQL dump.
+ */
+var copyDatabase = function(baseDatabase, newDatabase, dumpPath) {
+  // Set the full path to the dump file.
+  var dumpFile = dumpPath + '/' + baseDatabase + '.sql';
+
+  // Connect to the database.
+  var connection = mysql.createConnection({
+    host: db.host,
+    user: db.user,
+    password: db.password
+  });
+
+  // Start by creating a SQL dump.
+  exec('mysqldump ' + baseDatabase + ' -h ' + db.host + ' -u ' + db.user + ' -p' + db.password + ' > ' + dumpFile, function(error, stdout, stderr) {
+    if (error) return;
+    // Create the new database.
+    connection.query('CREATE DATABASE `' + newDatabase + '`', function(error, rows) {
+      if (error) return;
+      // Import the SQL dump to the new database.
+      exec('mysql ' + newDatabase + ' -h ' + db.host + ' -u ' + db.user + ' -p' + db.password + ' < ' + dumpFile, function(error, stdout, stderr) {
+        // Remove the SQL dump when everything is done.
+        fs.remove(dumpFile);
+      });
+
+    });
+  });
+}
+
+/**
+ * Drop a database.
+ *
+ * @param string database
+ *   The name of the database that should be deleted.
+ */
+var dropDatabase = function(database) {
+  // Connect to the database.
+  var connection = mysql.createConnection({
+    host: db.host,
+    user: db.user,
+    password: db.password
+  });
+
+  // Drop the database.
+  connection.query('DROP DATABASE `' + database + '`');
+}
+
 /**
  * Handles the create_branch event from Beanstalk.
  *
@@ -7,12 +69,6 @@
 module.exports.create = function(payload, callback) {
   // Assume success.
   var result = {status: 200, message: 'OK'};
-
-  // Load fs-extra in order to copy files.
-  var fs = require('fs-extra');
-
-  // Create the client which will handle the communication with Beanstalk.
-  var client = require('./client.js');
 
   // Get the environments for this repository.
   client.get(payload.repository.id + '/server_environments.json', function(error, response, body) {
@@ -30,11 +86,20 @@ module.exports.create = function(payload, callback) {
         var server = body[0].release_server;
 
         // Copy the existing folder to the new path.
-        var newPath = server.remote_path + '-f-' + payload.name.replace(/^.+\//g, '');
+        var newName = '-f-' + payload.name.replace(/^.+\//g, '');
+        var newPath = server.remote_path + newName;
+
         fs.copy(server.remote_path, newPath, function(error) {
           // Remove the settings file when the files has been copied, since
           // this will need to be configured for a new database.
           fs.remove(newPath + '/sites/default/settings.local.php');
+
+          // Determine the name of the database based on the folder for the site.
+          var baseDatabase = server.remote_path.replace(/^.+\//g, '');
+          var newDatabase = baseDatabase + newName;
+
+          // Copy the database.
+          copyDatabase(baseDatabase, newDatabase, newPath);
         });
 
         // Create the new environment.
@@ -122,8 +187,12 @@ module.exports.delete = function(payload, callback) {
         // environment since Beanstalk's API doesn't allow this :(
         fs.remove(server.remote_path);
 
+        // Drop the database.
+        var database = server.remote_path.replace(/^.+\//g, '');
+        dropDatabase(database);
+
         // Return the result.
-        result.message = 'Deleted the ' + server.remote_path + ' folder.';
+        result.message = 'Deleted the ' + server.remote_path + ' folder and the ' + database + ' database.';
         callback(result);
       });
     }
---
+++ app/db.js.default.js
@@ -0,0 +1,6 @@
+// Database credentials. Change these to match the details for your database.
+module.exports = {
+  host: '',
+  user: '',
+  password: ''
+};
